9e86a3659f6c957969ddb9649b1d29e84fa0a1cd
Gabriel Moreno <ganimomu2001@gmail.com>
2024-10-19 16:34:38 -0300

    change

--- apps/music-bot/src/bootstrap/web.ts
+++
@@ -1,7 +0,0 @@
-import { app } from '#bot/web/index';
-
-const PORT = Number(process.env.PORT);
-
-app.listen(PORT, () => {
-  console.log(`Web server listening on port: *${PORT}`);
-});
--- apps/music-bot/src/events/ready/index.ts
+++
@@ -1,5 +0,0 @@
-import type { Client } from "discord.js";
-
-export default (client: Client<true>) => {
-    console.log(`Logged in as ${client.user.tag}`);
-}
